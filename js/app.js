94a39f1902608f4ba387492ac0d9da0478ded87b
Filippo Massarelli <filippo.massarelli@gmail.com>
2020-07-23 18:23:46 +0200

    Active sections highlight onscroll when in view, links in navbar highlight too. Css color palette changed. Minor tweaks in html content.

--- js/app.js
+++ js/app.js
@@ -17,8 +17,8 @@
  * Define Global Variables
  *
  */
-const navItems = document.getElementsByTagName("section");
-
+const navItems = Array.from(document.getElementsByTagName("section"));
+const navUl = document.querySelector("#navbar__list");
 /**
  * End Global Variables
  * Start Helper Functions
@@ -32,11 +32,57 @@ const navItems = document.getElementsByTagName("section");
  */
 
 // build the nav
+for (navItem of navItems) {
+  console.log(navItem);
 
-// Add class 'active' to section when near top of viewport
+  let li = document.createElement("li");
+  let a = document.createElement("a");
 
-// Scroll to anchor ID using scrollTO event
+  a.classList.add("menu__link");
+  a.setAttribute("href", `#${navItem.id}`);
+  a.innerText = navItem.id;
 
+  li.appendChild(a);
+  navUl.appendChild(li);
+}
+
+// // Add class 'active' to section when near top of viewport
+// window.addEventListener("scroll", testFunction);
+
+// function testFunction() {
+//   const allSections = document.querySelectorAll("section");
+//   for (i = 0; i < allSections.length; i++) {
+//     if (!isInViewport(allSections[i])) {
+//       allSections[i].classList.remove("active");
+//     } else {
+//       allSections[i].classList.add("active");
+//     }
+//   }
+// }
+
+// function isInViewport(elem) {
+//   // function to define the sections' position in viewport
+//   let bounding = elem.getBoundingClientRect();
+//   return (
+//     bounding.top + 300 >= 0 &&
+//     bounding.left >= 0 &&
+//     bounding.bottom <=
+//       (window.innerHeight || document.documentElement.clientHeight) + 300 &&
+//     bounding.right <=
+//       (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// }
+
+// // Scroll to anchor ID using scrollTO event
+// document.querySelectorAll("a").forEach((a) => {
+//   a.addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     document.getElementById(a.getAttribute("href")).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
 /**
  * End Main Functions
  * Begin Events
@@ -48,3 +94,69 @@ const navItems = document.getElementsByTagName("section");
 // Scroll to section on link click
 
 // Set sections as active
+
+// ----------------------
+// ----------------------
+
+//
+// --> Highlight active section in body and in navbar on Scroll
+//
+
+const navbarLinks = document.querySelectorAll("li a");
+
+window.addEventListener("scroll", (event) => {
+  let fromTop = window.scrollY + 1;
+
+  navbarLinks.forEach((link) => {
+    let section = document.querySelector(link.getAttribute("href"));
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("active-link");
+      section.classList.add("your-active-class");
+    } else {
+      link.classList.remove("active-link");
+      section.classList.remove("your-active-class");
+    }
+  });
+});
+
+//
+// --> Scroll to anchor ID with ScrollTo
+//
+
+document.querySelectorAll("a").forEach((a) => {
+  a.addEventListener("click", function (prvt) {
+    prvt.preventDefault();
+    // console.log("ScrollTo worked");
+    document.getElementById(a.innerText).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+//
+// --> Scroll To Top button
+//
+
+const topButton = document.getElementById("topButton");
+
+//when user scrolls 20px down from top
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+//Go to top on click
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
